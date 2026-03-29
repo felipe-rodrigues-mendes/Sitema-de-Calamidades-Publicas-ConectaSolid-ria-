5819b4d49adf7a48059fc2673fcfc4d81ab7c4b9
Felipe Rodrigues Mendes <felipeetc22@gmail.com>
2026-03-29 20:18:04 -0300

    Fix HTML encoding and improve contact form handling

--- contato.php
+++ contato.php
@@ -49,7 +49,7 @@ label::after {
 <nav>
     <a href="index.php"><i class="fas fa-home"></i> Início</a> 
     <a href="cadastro.php"><i class="fa fa-user"></i> Cadastro</a> 
-     <a href="doação"><i class="fas fa-hand-holding-heart"></i> Doação</a>
+    <a href="doacao.php"><i class="fas fa-hand-holding-heart"></i> Doação</a>
     <a href="contato.php"><i class="fas fa-envelope"></i> Contato</a> 
     <a href="sobre.php"><i class="fas fa-info-circle"></i> Sobre</a> 
 </nav>
